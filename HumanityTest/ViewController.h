672fd53dbadd54775e970dd4ad7ae8c6133691db
Dejan Bekic <dejanbekic@Dejans-MacBook-Pro.local>
2015-07-04 14:39:52 +0200

    Initial Commit

---
+++ HumanityTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HumanityTest
+//
+//  Created by Dejan Bekic on 7/4/15.
+//  Copyright (c) 2015 Dejan Bekic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
